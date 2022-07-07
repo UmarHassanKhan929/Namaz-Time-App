6f2a8d0c3958da8933254a6b701a81672deb04f6
Umar Hassan Khan <khanumarhassan26@gmail.com>
2022-07-07 12:28:23 +0500

    api calls for time added

--- lib/screens/timings.dart
+++ lib/screens/timings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Timings extends StatefulWidget {
   const Timings({Key? key}) : super(key: key);
@@ -10,10 +11,177 @@ class Timings extends StatefulWidget {
 }
 
 class _TimingsState extends State<Timings> {
+  final _cityController = TextEditingController();
+  var _fetchedData = <String, dynamic>{};
+  var _isLoading = false;
+  late Map<String, dynamic> apiData;
+
+  Future<void> _submitForm() async {
+    if (!_isLoading) {
+      setState(() {
+        _isLoading = true;
+      });
+    }
+
+    final response = await http.get(Uri.parse(
+        'https://dailyprayer.abdulrcs.repl.co/api/${_cityController.text.trim()}'));
+    final reponseData = await jsonDecode(response.body);
+    setState(() {
+      _isLoading = false;
+      _fetchedData = reponseData;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Timings here'),
+    return SingleChildScrollView(
+      child: Container(
+        margin: const EdgeInsets.all(14),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            TextField(
+              controller: _cityController,
+              onSubmitted: (value) {
+                _submitForm();
+              },
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color.fromARGB(255, 58, 85, 106),
+                decoration: TextDecoration.none,
+              ),
+              // onSubmitted is called when the user hits enter
+              decoration: InputDecoration(
+                isDense: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                prefixIcon: const Icon(
+                  Icons.search_rounded,
+                  size: 32,
+                ),
+                hintText: 'Search City',
+              ),
+            ),
+            if (_isLoading)
+              const Padding(
+                padding: EdgeInsets.all(7.0),
+                child: Center(
+                  child: LinearProgressIndicator(
+                    minHeight: 2,
+                  ),
+                ),
+              ),
+            const SizedBox(
+              height: 20,
+            ),
+            if (!_isLoading && _fetchedData.isNotEmpty)
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '${_fetchedData['city']}',
+                        style: const TextStyle(
+                          fontSize: 32,
+                          color: Color.fromARGB(255, 58, 85, 106),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        '${_fetchedData['date']}',
+                        style: const TextStyle(
+                          height: 3,
+                          fontSize: 14,
+                          color: Color.fromARGB(255, 58, 85, 106),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  NamazTime(
+                      timefetched: _fetchedData['today']['Fajr'],
+                      namaz: 'Fajr'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  NamazTime(
+                      timefetched: _fetchedData['today']['Sunrise'],
+                      namaz: 'Sunrise'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  NamazTime(
+                      timefetched: _fetchedData['today']['Dhuhr'],
+                      namaz: 'Dhuhr'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  NamazTime(
+                      timefetched: _fetchedData['today']['Asr'], namaz: 'Asr'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  NamazTime(
+                      timefetched: _fetchedData['today']['Maghrib'],
+                      namaz: 'Maghrib'),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  NamazTime(
+                      timefetched: _fetchedData['today']["Isha'a"],
+                      namaz: "Isha'a"),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NamazTime extends StatelessWidget {
+  NamazTime({
+    required this.namaz,
+    required this.timefetched,
+  }) {
+    time = TimeOfDay(
+        hour: int.parse(timefetched.split(":")[0]),
+        minute: int.parse(timefetched.split(":")[1]));
+  }
+
+  final String namaz;
+  final String timefetched;
+
+  late TimeOfDay time;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          namaz,
+          style: const TextStyle(
+            fontSize: 28,
+            color: Color.fromARGB(255, 58, 85, 106),
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        Text(
+          time.format(context),
+          style: const TextStyle(
+            fontSize: 28,
+            color: Color.fromARGB(255, 58, 85, 106),
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ],
     );
   }
 }
